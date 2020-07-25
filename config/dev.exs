edb1ef10012a9305978387a656442de745787879
kamranhossain <kamran.hossain227@gmail.com>
2020-07-25 16:29:29 +0600

    USER_DB and USER_PASSWORD getting from .env file in dev and test conf

--- config/dev.exs
+++ config/dev.exs
@@ -2,12 +2,14 @@ use Mix.Config
 
 # Configure your database
 config :auth, Auth.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "auth_dev",
+  adapter: Ecto.Adapters.Postgres,
   hostname: "localhost",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASSWORD") || "",
+  database: "auth_dev",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  log_level: :debug
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :auth, Auth.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASSWORD") || "",
   database: "auth_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
